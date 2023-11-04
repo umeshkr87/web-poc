ff48964ef20c5bcee863aaa94fcfc6a1cc179de2
Umesh Kumar <umesh2@illinois.edu>
2023-11-04 16:28:04 -0400

    Test

--- poc.js
+++ poc.js
@@ -4,5 +4,11 @@ function redirectToApp() {
 
     window.location = 'visible://dummyparam';
 
-    
+    // const iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
+
+    // if (iOS) {
+    // // Just replace `https://` with `itms://` on your app's AppStore link.
+    // window.location.href = "itms://itunes.apple.com/us/app/google-maps-transit-food/id585027354?mt=8";
+    // }
+
 }
