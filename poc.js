6ad3ef19296a40caf393451e42191df7bb0794a5
Umesh Kumar <umesh2@illinois.edu>
2023-11-04 20:22:09 -0400

    testing

--- poc.js
+++ poc.js
@@ -7,7 +7,13 @@ function redirectToApp() {
     // const iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
     // alert(navigator.userAgentData.platform);
 
-    // const iOS = navigator.userAgentData.platform === 'iOS';
+    const iOS = navigator.userAgentData.platform === 'iOS';
+    if (!iOS) {
+        console.log("Not an iOS platform");
+        return;
+    }
+        
+
     // if (iOS) {
     
     //     window.location = 'visible://dummyparam';
@@ -18,23 +24,42 @@ function redirectToApp() {
     // }
 
 
+    // Another way 
+    // (function() {
+    //     var app = {
+    //       launchApp: function() {
+    //         window.location.replace("visible://dummyparam");
+    //         this.timer = setTimeout(this.openAppStore, 1000);
+    //       },
+      
+    //       openAppStore: function() {
+    //         window.location.replace("https://apps.apple.com/us/app/visible-mobile/id1367950045?ls=1");
+    //       }
+    //     };
+      
+    //     app.launchApp();
+    //   })();
 
-    (function() {
-        var app = {
-          launchApp: function() {
-            window.location.replace("visible://dummyparam");
-            this.timer = setTimeout(this.openWebApp, 1000);
-          },
-      
-          openWebApp: function() {
-            window.location.replace("https://apps.apple.com/us/app/visible-mobile/id1367950045?ls=1");
-          }
-        };
-      
-        app.launchApp();
-      })();
 
-      
+      var redirect = function (location) {
+        var iframe = document.createElement('iframe');
+        iframe.setAttribute('src', location);
+        iframe.setAttribute('width', '1px');
+        iframe.setAttribute('height', '1px');
+        iframe.setAttribute('position', 'absolute');
+        iframe.setAttribute('top', '0');
+        iframe.setAttribute('left', '0');
+        document.documentElement.appendChild(iframe);
+        iframe.parentNode.removeChild(iframe);
+        iframe = null;
+    };
+    
+    setTimeout(function () {
+        redirect('https://apps.apple.com/us/app/visible-mobile/id1367950045?ls=1');
+    }, 25);
+    
+    redirect('visible://dummyparam');
+
 }
 
 
